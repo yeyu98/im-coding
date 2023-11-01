ec099efd1e5d6627ce936d6a30d055ec88a3a2ba
lzy-Jerry <763626908@qq.com>
2023-11-02 00:40:22 +0800

    feat: 移除样式

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,15 @@
  * @Author: lzy-Jerry
  * @Date: 2023-10-31 20:40:08
  * @LastEditors: lzy-Jerry
- * @LastEditTime: 2023-11-02 00:29:44
+ * @LastEditTime: 2023-11-02 00:39:44
  * @Description:
  */
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.less";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
