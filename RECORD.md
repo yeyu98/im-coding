3ed2e9d76ae7a78fdc7cf784157bf2db2e1dd8df
lzy-Jerry <763626908@qq.com>
2023-11-02 01:37:21 +0800

    feat: 运行进程

--- RECORD.md
+++ RECORD.md
@@ -2,7 +2,7 @@
  * @Author: lzy-Jerry
  * @Date: 2023-11-02 00:29:53
  * @LastEditors: lzy-Jerry
- * @LastEditTime: 2023-11-02 01:04:37
+ * @LastEditTime: 2023-11-02 01:33:12
  * @Description:
 -->
 
@@ -14,6 +14,8 @@
   - pnpm install @types/node --save-dev
 
 - 什么是shareArray，为什么使用它需要开启跨域隔离？
+- 配置跨域隔离之后打开浏览器出现 net::ERR_CACHE_READ_FAILURE，但开启无痕模式之后有可以正常访问？？
+  - 猜测是某些文件的缓存有关系
 
 # web container
 
@@ -24,3 +26,6 @@
     - Cross-Origin-Embedder-Policy: require-corp
     - Cross-Origin-Opener-Policy: same-origin
   - 为了安全考虑webcontainer只能运行在localhost & https之下；
+- 在webcontainer中通过mount加载文件 TODO 关于文件系统的后续补充
+- 设置命令以及运行一些进程启动devServer（这里需要基于上一步的文件系统）
+  - 这里主要实现的是npm install & npm run dev
--- src/components/WebContainer/WebContainer.tsx
+++ src/components/WebContainer/WebContainer.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { WebContainer as WebContainerClass } from "@webcontainer/api";
 import "./WebContainer.less";
 
@@ -6,17 +6,29 @@ interface Props {}
 
 function WebContainer(props: Props) {
   const {} = props;
+  const webcontainerInstance = useRef<InstanceType<typeof WebContainerClass>>();
 
   const createWebContainerInstance = async () => {
-    const webContainerInstance = await WebContainerClass.boot();
-    console.log(
-      "🚀🚀🚀 ~ file: WebContainer.tsx:12 ~ createWebContainerInstance ~ webContainerInstance--->>>",
-      webContainerInstance,
-    );
+    console.log("createWebContainerInstance");
+    webcontainerInstance.current = await WebContainerClass.boot();
+  };
+
+  const startDevServer = async () => {
+    console.log("startDevServer");
+    const installProcess = await webcontainerInstance.current?.spawn("npm", ["install"]);
+    // 检测进程运行状态是否退出
+    const installExitCode = await installProcess?.exit;
+    console.log("🚀🚀🚀 ~ file: WebContainer.tsx:20 ~ startDevServer ~ installExitCode--->>>", installExitCode);
+
+    if (installExitCode != 0) {
+      throw new Error("Unable to run npm install");
+    }
+    await webcontainerInstance.current?.spawn("npm", ["run", "dev"]);
   };
 
   useEffect(() => {
     createWebContainerInstance();
+    startDevServer();
   }, []);
 
   return (
