ffaa572cf26304c8b9ee25e40263a02119491cd2
xiaohu <zhouyi@yx.com>
2023-11-03 16:06:20 +0800

    feat: 添加注释

--- src/components/CodeEditor/components/Preview/Preview.tsx
+++ src/components/CodeEditor/components/Preview/Preview.tsx
@@ -2,11 +2,11 @@
  * @Author: xiaohu
  * @Date: 2023-11-02 17:18:49
  * @LastEditors: xiaohu
- * @LastEditTime: 2023-11-03 10:17:43
+ * @LastEditTime: 2023-11-03 16:05:39
  * @FilePath: \im-coding\src\components\CodeEditor\components\Preview\Preview.tsx
  * @Description:
  */
-import React from "react";
+// import React from "react";
 import styles from "./Preview.module.less";
 
 interface Props {
